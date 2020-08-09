41eb92a921354b0c6cf2cf720abae9ceaf7a48de
13020170208 <68625050+13020170208@users.noreply.github.com>
2020-08-10 03:51:18 +0800

    Add files via upload

---
+++ BelajarArray/src/Array/ContohArray.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Array;
+
+/**
+ *
+ * @author AfdalMaulana
+ */
+public class ContohArray {
+    public static void main(String[] args) {
+        String[] anggota = {"Afdal", "Maulana", "Afif", "Faje", "Ichlas"};
+        for(int x=0;x<anggota.length;x++){
+            System.out.println("Array Index Ke = "+x+"Adalah "+anggota[x]);
+        }
+    }
+}
